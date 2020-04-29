fce98fdb98d3984b5095e2af34120c1b9209f084
golu360 <golu360@gmail.com>
2020-04-29 21:39:19 +0530

    29/4

--- tracker/settings.py
+++ tracker/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'tracker.urls'
@@ -128,4 +128,4 @@ STATICFILES_DIRS = [
 
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
