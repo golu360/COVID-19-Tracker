9f4779beb602dd37f42faa32d87b5008c358d1bc
golu360 <golu360@gmail.com>
2020-04-29 21:00:43 +0530

    29/4

--- tracker/settings.py
+++ tracker/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +26,7 @@ SECRET_KEY = '59+@s1^etkkg@t^787&qca2m8_$ja3tho3-fm$x2a@cl7zaa%8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://covid-19-gandhinagar.herokuapp.com/']
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
