96fdef1e107d777907795cf04790bfc3ff20c116
golu360 <golu360@gmail.com>
2020-05-02 18:45:38 +0530

    2/5

--- track/views.py
+++ track/views.py
@@ -7,39 +7,6 @@ import os
 # Create your views here.
 
 
-'''
-def home(request):
-    dataset = District.objects.all()
-
-    district_names = list()
-    positives = list()
-    recovered = list()
-    tested = list()
-    deaths = list()
-    quarantined = list()
-
-
-    for district in dataset:
-        district_names.append(district.district_name)
-        positives.append(district.district_positive)
-        recovered.append(district.district_recovered)
-        tested.append(district.district_tested)
-        quarantined.append(district.district_quarantined)
-        deaths.append(district.district_deaths)
-
-    context = {
-        'names': json.dumps(district_names),
-        'positives':json.dumps(positives),
-        'recovered': json.dumps(recovered),
-        'tested':json.dumps(tested),
-        'deaths':json.dumps(deaths),
-        'quarantined':json.dumps(quarantined)
-    }    
-
-
-
-    return render(request,'home.html',context=context)
-'''
 
 
 def home(request):
@@ -106,3 +73,8 @@ def stats(request):
 
 
     return render(request,'statistics.html',context=context)
+
+
+
+def aboutus(request):
+    return render(request,'aboutus.html')
--- tracker/urls.py
+++ tracker/urls.py
@@ -21,5 +21,6 @@ import track.views as views
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^$',views.home,name='home'),
-    url('^stats/',views.stats,name='statistics')
+    url('^stats/',views.stats,name='statistics'),
+    url('^aboutus/',views.aboutus,name='aboutus')
 ]
